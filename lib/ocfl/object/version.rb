0548f4c202a046d357ced8f5fe2cc63e84caa708
Justin Coyne <jcoyne@justincoyne.com>
2024-03-12 07:32:01 -0500

    Add message and user to version

--- lib/ocfl/object/version.rb
+++ lib/ocfl/object/version.rb
@@ -5,9 +5,18 @@ module OCFL
     # Represents the OCFL version
     # https://ocfl.io/1.1/spec/#version
     class Version < Dry.Struct
+      # Represents the OCFL user
+      class User < Dry.Struct
+        transform_keys(&:to_sym)
+        attribute :name, Types::String
+        attribute? :address, Types::String
+      end
+
       transform_keys(&:to_sym)
       attribute :created, Types::String
       attribute :state, Types::Hash.map(Types::String, Types::Array.of(Types::String))
+      attribute? :message, Types::String
+      attribute? :user, User
     end
   end
 end
